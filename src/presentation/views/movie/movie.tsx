28aab2dd7046f4fb27ee18999fcd11bd1977e78e
André Mafioletti <and.mafioletti@gmail.com>
2025-09-20 10:54:01 -0300

    fix: corrigi o layout da pagina movie

--- src/presentation/views/movie/movie.tsx
+++ src/presentation/views/movie/movie.tsx
@@ -1,24 +1,24 @@
 import { Col, Row } from "antd";
-import { useGetMovieById } from "../../../application/queries/get-movie-byid/useGetMovieById";
+import { useGetMovieById } from "../../../application/queries/get-movie-by-id/useGetMovieById";
 export default function Movie() {
     const { data, isLoading } = useGetMovieById();
     return (
-    <section className="movie-detail">
-    <div className="container">
-    {isLoading && <h1>Loading...</h1>}
-    <Row>
-    <Col span={24}>
-    <h1>{data?.title}</h1>
-    {data?.poster_path && (
-    <img
-    src={`https://image.tmdb.org/t/p/w400${data.poster_path}`}
-    alt=""
-    />
-    )}
-    <p>{data?.overview}</p>
-    </Col>
-    </Row>
-    </div>
-    </section>
+        <section className="movie-detail">
+            <div className="container">
+                {isLoading && <h1>Loading...</h1>}
+                <Row>
+                    <Col span={24}>
+                        <h1>{data?.title}</h1>
+                        {data?.poster_path && (
+                            <img
+                                src={`https://image.tmdb.org/t/p/w400${data.poster_path}`}
+                                alt=""
+                            />
+                        )}
+                        <p>{data?.overview}</p>
+                    </Col>
+                </Row>
+            </div>
+        </section>
     );
-    }
+}
